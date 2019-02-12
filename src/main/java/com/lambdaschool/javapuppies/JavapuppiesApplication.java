294063aabc6c3febefbfe3ca4ed12733674034c9
Patrick Martin <patrickmartin88@gmail.com>
2019-02-12 15:16:54 -0800

    Initial Commit

---
+++ src/main/java/com/lambdaschool/javapuppies/JavapuppiesApplication.java
@@ -0,0 +1,14 @@
+package com.lambdaschool.javapuppies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavapuppiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavapuppiesApplication.class, args);
+    }
+
+}
+
